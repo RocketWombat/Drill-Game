f0dba43b5b0402d1330a349921e0bed248c368a8
Jdrs132 <jacobdipietro1@gmail.com>
2024-06-23 17:10:16 -0500

    Some more additions

--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 
 var screen_size
@@ -6,19 +7,33 @@ var screen_size
 func _ready():
 	screen_size = get_viewport_rect().size
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#Every frame we need to do the following
-	#	Update the players speed (only X axis)
-	#	Update the players location
-		
 		
 	#Update Players Speed
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") && Input.is_action_pressed("right"):
+		velocity.x = 0
+	elif Input.is_action_pressed("left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right"):
 		velocity.x += 1
 	else:
+		velocity.x = 0
 		
+	velocity = velocity * speed
+	
+	#Update player position
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionPolygon2D.disabled = false
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionPolygon2D.set_deferred("disabled",true)
