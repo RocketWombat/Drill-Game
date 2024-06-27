2532972efc84a4deaf9491d3fa6367b1b4d21906
Skymiles56 <120430312+Skymiles56@users.noreply.github.com>
2024-06-27 17:29:27 -0400

    Add files via upload

---
+++ Hub.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+signal start_game
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func game_over():
+	$StartButton.show()
+	
+	
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit() 
